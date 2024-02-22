8d88b0b1175de26ba55e70f91938b6c7a612f7ba
HET MEHTA <160310708+HET-MEHTA@users.noreply.github.com>
2024-02-22 10:45:21 +0530

    Add files via upload
    HERE THERE IS THE BASIC JAVA HELLO WORLD PROGRAM!

---
+++ hellop1.java
@@ -0,0 +1,12 @@
+class booltest
+{
+    public static void main(String arg[])
+    {
+            boolean b;
+            b=false;
+            System.out.println("b is " +b);
+             b=true;
+            System.out.println("b is " +b);
+            
+    }
+            }
